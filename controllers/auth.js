10e6116c752cda907f0dbca0e47dfbd263c0eea1
David Okaliwe <okaliwedavid@gmail.com>
2024-04-08 21:24:46 +0100

    Updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,21 @@ import Admin from '../models/admin.js';
 
 export const login = async (req, res) => {
 	try {
-		const newAdminData = {
-            email: "admin@epjournals.com",
-            password: "admin@epjournals_24",
-        };
+        const {
+			email,
+			password,
+		} = req.body;
 
-        const newAdmin = new Admin(newAdminData);
-        await newAdmin.save();
+		const salt = await bcrypt.genSalt();
+		const passwordHash = await bcrypt.hash(password, salt);
 
-        return new Response(JSON.stringify(newAdmin), {
-            status: 201,
-        });
+		const newAdmin = new Admin({
+			email,
+			password: passwordHash,
+		});
+
+		const savedAdmin = await newAdmin.save();
+		res.status(201).json(savedAdmin);
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
