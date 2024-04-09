e3cfbb08a20294a765fb075a5c6b6dd4acd6bc24
David Okaliwe <okaliwedavid@gmail.com>
2024-04-09 01:28:13 +0100

    Updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,13 @@ import Admin from '../models/admin.js';
 
 export const login = async (req, res) => {
 	try {
-        const {
-			email,
-			password,
-		} = req.body;
+		const { email, password } = req.body;
+
+		// Check if an admin with the same email already exists
+		const existingAdmin = await Admin.findOne({ email });
+		if (existingAdmin) {
+			return res.status(400).json({ error: "An admin with this email already exists" });
+		}
 
 		const salt = await bcrypt.genSalt();
 		const passwordHash = await bcrypt.hash(password, salt);
